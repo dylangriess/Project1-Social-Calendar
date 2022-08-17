e5d26ba87b6e972f5cf96075d2e4c35b49cf4c5a
Martin-Borchert <108767551+ConnerMart@users.noreply.github.com>
2022-08-17 11:55:54 -0600

    added brewery tour div with js

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+var showBreweryBtn = document.getElementById("showBreweryBtn");
+var breweryName = document.getElementById("breweryName");
+var webSpan = document.getElementById("breweryWeb");
+var phoneSpan = document.getElementById("breweryPhone");
+
+showBreweryBtn.addEventListener("click", showBrewery);
+
+var breweriesDenver =
+  "https://api.openbrewerydb.org/breweries?by_city=Denver&per_page=50";
+
+function showBrewery() {
+  fetch(breweriesDenver)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      rand = Math.floor(Math.random() * data.length);
+      breweryName.textContent = data[rand].name;
+      webSpan.textContent = data[rand].website_url;
+      phoneSpan.textContent = data[rand].phone;
+      if (data[rand].website_url === null) {
+        webSpan.textContent = "website not available";
+      }
+      if (data[rand].phone === null) {
+        phoneSpan.textContent = "phone not available";
+      }
+    });
+}
